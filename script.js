937d4d6d59a66a285b5684eebc7b8903746bb21f
Faddah Wolf <my_biz@me.com>
2025-01-27 15:45:46 -0800

    Commit of script.js file, working as expected.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log(`TypeScript & JavaScript are loaded`);
+
+const t = document.querySelector('title');
+const h1 = document.querySelector('.page-title');
+const pageTitle = `Page Background Color Picker Extension`;
+t.textContent = pageTitle;
+h1.textContent = "Hello!";
+
+async function sayHello() {
+	let [tab] = await chrome.tabs.query({ active: true });
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: () => {
+			// document.body.style.backgroundColor = 'red';
+			console.log('Hello from my extension!');
+			alert('Hello from my extension!');
+		},
+	});
+}
+
+document.querySelector('#myButton').addEventListener('click', sayHello);
