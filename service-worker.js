14abb17c39b790b40e0d6b1aea1a5ddc81740667
Faddah Wolf <my_biz@me.com>
2025-01-27 15:41:54 -0800

    Commented out most of service-worker.js code as it is no longer needed.

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+// Uses the Chrome API
+console.log(`Service Worker is loaded...`);
+// chrome.action.onClicked.addListener((tab) => {
+// 		chrome.scripting.executeScript({
+// 				target: { tabId: tab.id },
+// 				function: () => {
+// 						// document.body.style.backgroundColor = 'red';
+// 						alert('Hello from my service-worker in my extension!');
+// 						console.log('Hello from my service-worker in my extension!');
+// 				},
+// 		});
+// });
